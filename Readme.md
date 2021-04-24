af7fed224d8db7041c115eca1e655908d8b811d7
shankaryadav <shankaryadav0130@gmail.com>
2021-04-24 22:31:03 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# A Basic Android Group Chat App using Firebase Realtime Database
+
+![image not found](https://i.imgur.com/MkmgKe4.jpg)
+![image not found](https://i.imgur.com/pCnZcQS.jpg)
+![image not found](https://i.imgur.com/yXdgcZo.jpg)
---
+++ app/src/main/java/com/example/codechatandy/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.codechatandy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String myUserName;
+    private ListView myChatListView;
+    private EditText myChatText;
+    public ImageButton mySendChatButton;
+    private DatabaseReference myDataBaseRef;
+
+    private  ChatListAdapter myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        setUpDisplayName();
+        myDataBaseRef = FirebaseDatabase.getInstance().getReference();
+
+// Get the reference of UI Elements
+        myChatListView = (ListView) findViewById(R.id.chat_listView);
+        myChatText = (EditText) findViewById(R.id.messageInput);
+        mySendChatButton = (ImageButton) findViewById(R.id.sendButton);
+
+        // push chat to FireBase button tab
+        mySendChatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pushChatToFirebase();
+            }
+        });
+
+        //call the pushChatToFireBase on keyboard event
+
+        myChatText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                pushChatToFirebase();
+                return true;
+            }
+        });
+    }
+
+
+    //push chat to Firebase
+
+    private void pushChatToFirebase(){
+        String chatInput = myChatText.getText().toString();
+        if(!chatInput.equals("")){
+            InstantMessage chat = new InstantMessage(chatInput,myUserName);
+            myDataBaseRef.child("chats").push().setValue(chat);
+            myChatText.setText("");
+        }
+    }
+    // set username for user
+
+    private void setUpDisplayName(){
+        SharedPreferences prefs = getSharedPreferences(RegisterActivity.CHAT_Pref,MODE_PRIVATE);
+        myUserName = prefs.getString(RegisterActivity.DISPLAY_NAME, null);
+
+        if(myUserName == null){
+            myUserName = "user";
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myAdapter = new ChatListAdapter(MainActivity.this,myDataBaseRef,myUserName);
+        myChatListView.setAdapter(myAdapter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        myAdapter.freeUpResources();
+    }
+}
